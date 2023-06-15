fed0c263ab3de1eb2d0336a8a99a6b35b7399718
Ryan Wade <ryanwade@Ryans-MBP.lan>
2023-06-15 16:08:33 -0500

    Rick And Morty Universe

---
+++ src/App.js
@@ -0,0 +1,34 @@
+// import logo from './logo.svg';
+import './App.css';
+import CharacterList from './CharacterList';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Nav from './Nav';
+import NotFoundPage from './NotFoundPage';
+import LocationList from './LocationList';
+import HomePage from './HomePage';
+import CharacterDetails from './CharacterDetails';
+import RandomCharacter from './RandomCharacter';
+import Footer from './Footer';
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Nav />
+      <div className="container-fluid" style={{ backgroundColor: '#26b1c6', minHeight: '100vh', display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
+        <Routes>
+
+          <Route path="/" element={<HomePage />} />
+          <Route path="/characters" element={<CharacterList />} />
+          <Route path="/characters/:id" element={<CharacterDetails />} />
+          <Route path="/locations" element={<LocationList />} />
+          <Route path="/random" element={<RandomCharacter />} />
+          <Route path="*" element={<NotFoundPage />} />
+        </Routes>
+      </div>
+      <Footer />
+    </BrowserRouter>
+  );
+}
+export default App;
---
+++ src/Footer.js
@@ -0,0 +1,65 @@
+import React from 'react';
+
+const Footer = () => {
+  const footerStyle = {
+    backgroundColor: '#f1f1f1',
+    padding: '20px',
+  };
+
+  const containerStyle = {
+    display: 'flex',
+    flexWrap: 'wrap',
+    justifyContent: 'space-between',
+    alignItems: 'flex-start',
+  };
+
+  const columnStyle = {
+    flex: '1',
+    minWidth: '200px',
+    marginBottom: '20px',
+  };
+
+  const bottomStyle = {
+    width: '100%',
+    textAlign: 'center',
+    marginTop: '20px',
+  };
+
+  return (
+    <footer style={footerStyle}>
+      <div style={containerStyle}>
+        <div style={columnStyle}>
+          <h4>
+            <img src="/rmlogo.png" alt="Rick and Morty Logo" />
+          </h4>
+          <ul>
+            <li>This website was made using</li>
+            <li>the Rick and Morty Api at</li>
+            <li><a href="https://rickandmortyapi.com/" target="_blank" rel="noopener noreferrer">https://rickandmortyapi.com/</a></li>
+          </ul>
+        </div>
+        <div style={columnStyle}>
+          <h4></h4>
+          <ul>
+            <li></li>
+            <li></li>
+            <li></li>
+          </ul>
+        </div>
+        <div style={columnStyle}>
+          <h4></h4>
+          <ul>
+            <li></li>
+            <li></li>
+            <li></li>
+          </ul>
+        </div>
+      </div>
+      <div style={bottomStyle}>
+        <p>&copy; {new Date().getFullYear()} Rick and Morty Universe</p>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
