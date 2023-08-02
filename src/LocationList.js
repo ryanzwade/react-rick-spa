7d27f6d7d8528e820193eaeff434267bf71f957d
Ryan Wade <ryanwade@Ryans-MBP.lan>
2023-08-02 00:21:51 -0500

    transfer from gitlab to github

--- src/LocationList.js
+++ src/LocationList.js
@@ -10,7 +10,7 @@ function LocationList(props) {
       const data = await response.json();
       console.log(data);
       setLocations(data.results);
-    }
+    } // Add a closing curly brace here
   }
 
   useEffect(() => {
@@ -19,31 +19,31 @@ function LocationList(props) {
 
   return (
     <div className="container App">
-    <img
-      src="https://preview.redd.it/o6cwlzg3exk41.png?width=960&crop=smart&auto=webp&v=enabled&s=e29c84013ab4bb1bf5a51ded8b4b31ff478a9feb"
-      alt="Rick and Morty"
-    />
-    <h1 className="custom-font" style={{ fontSize: '350%' }}>Rick and Morty Location List</h1>
-    <table className="table table-success table-striped">
-      <thead>
-        <tr>
-          <th>Location Name</th>
-          <th>Type</th>
-          <th>Dimension</th>
-        </tr>
-      </thead>
-      <tbody>
-        {locations.map((location) => {
-          return (
-            <tr key={location.id}>
-              <td>{location.name}</td>
-              <td>{location.type}</td>
-              <td>{location.dimension}</td>
-            </tr>
-          );
-        })}
-      </tbody>
-    </table>
+      <img
+        src="https://preview.redd.it/o6cwlzg3exk41.png?width=960&crop=smart&auto=webp&v=enabled&s=e29c84013ab4bb1bf5a51ded8b4b31ff478a9feb"
+        alt="Rick and Morty"
+      />
+      <h1 className="custom-font" style={{ fontSize: '350%' }}>Rick and Morty Location List</h1>
+      <table className="table table-success table-striped">
+        <thead>
+          <tr>
+            <th>Location Name</th>
+            <th>Type</th>
+            <th>Dimension</th>
+          </tr>
+        </thead>
+        <tbody>
+          {locations.map((location) => {
+            return (
+              <tr key={location.id}>
+                <td>{location.name}</td>
+                <td>{location.type}</td>
+                <td>{location.dimension}</td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
     </div>
   );
 }
