500fb13cc2a52ae33186ce9e26468da09529c70a
Ryan Wade <ryanwade@Ryans-MBP.lan>
2023-08-09 00:03:53 -0500

    last changes, bugs killed

--- src/CharacterDetails.js
+++ src/CharacterDetails.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { useParams, Link } from "react-router-dom";
+import { useParams} from "react-router-dom";
 
 function CharacterDetails() {
   const { id } = useParams();
@@ -67,4 +67,3 @@ function CharacterDetails() {
 }
 
 export default CharacterDetails;
-
